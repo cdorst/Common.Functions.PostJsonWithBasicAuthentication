30bc7837f57a6ee705688ffa4c12312a60c5c7a9
Christopher Dorst <chris@dorst.email>
2018-01-03 01:13:46 -0600

    Add BasicAuthJsonApiPoster

---
+++ Common.Functions.PostJsonWithBasicAuthentication/BasicAuthJsonApiPoster.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Common.Functions.PostJsonWithBasicAuthentication
+{
+    public static class BasicAuthJsonApiPoster
+    {
+        public static async Task<HttpResponseMessage> Post(ApiRequest request, BasicAuthenticationCredentials credentials)
+        {
+            using (var client = new HttpClient { BaseAddress = request.BaseAddress })
+            {
+                client.DefaultRequestHeaders.Authorization = credentials.Header;
+                return await client.SendAsync(request.Message);
+            }
+        }
+    }
+}
