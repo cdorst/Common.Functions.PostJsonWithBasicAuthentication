f944100159dc590e5a319e71acd23c0ea823fc17
Christopher Dorst <chris@dorst.email>
2018-01-03 04:18:46 -0600

    Moving BasicAuthenticationCredentials and ApiRequest into new namespace

--- Common.Functions.PostJsonWithBasicAuthentication/BasicAuthJsonApiPoster.cs
+++ Common.Functions.PostJsonWithBasicAuthentication/BasicAuthJsonApiPoster.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿using Common.Structures.HttpBasicAuthentication;
+using Common.Structures.HttpJsonRequest;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Common.Functions.PostJsonWithBasicAuthentication
@@ -7,6 +10,7 @@ namespace Common.Functions.PostJsonWithBasicAuthentication
     {
         public static async Task<HttpResponseMessage> Post(ApiRequest request, BasicAuthenticationCredentials credentials)
         {
+            if (request == null) throw new ArgumentNullException(nameof(request));
             using (var client = new HttpClient { BaseAddress = request.BaseAddress })
             {
                 client.DefaultRequestHeaders.Authorization = credentials.Header;
